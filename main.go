59c00b80697ea77052940485577d6b9cf74bf2f0
Adam Enger <adamenger@gmail.com>
2023-10-07 08:44:48 -0500

    adding filebeat handler

--- main.go
+++ main.go
@@ -21,6 +21,10 @@ type LogData struct {
   ParsedMessage map[string]interface{} `json:"parsedMessage,omitempty"` // will be omitted from JSON if empty
 }
 
+type FilebeatHandler struct {
+  Grok *grok.Grok
+}
+
 func ingestHandler(w http.ResponseWriter, r *http.Request) {
   // Ensure it's a POST request
   if r.Method != "POST" {
@@ -42,6 +46,63 @@ func ingestHandler(w http.ResponseWriter, r *http.Request) {
   if len(data.Tags) == 0 || data.Message == "" {
     http.Error(w, "Invalid data format", http.StatusBadRequest)
     return
+  }
+
+  // Convert the struct back to JSON for Kafka
+  message, err := json.Marshal(data)
+  if err != nil {
+    http.Error(w, "Failed to marshal data", http.StatusInternalServerError)
+    return
+  }
+
+  // Produce the message to Kafka
+  err = produceToKafka(message)
+  if err != nil {
+    http.Error(w, fmt.Sprintf("Failed to produce message to Kafka: %v", err), http.StatusInternalServerError)
+    return
+  }
+
+  w.Write([]byte("Message ingested successfully"))
+}
+
+func (f *FilebeatHandler) HandleRequest(w http.ResponseWriter, r *http.Request) {
+  // Ensure it's a POST request
+  if r.Method != "POST" {
+    http.Error(w, "Only POST method is supported", http.StatusBadRequest)
+    return
+  }
+
+  // Assuming the Filebeat payload is a nested JSON
+  // with metadata and a message field
+  var payload map[string]interface{}
+  decoder := json.NewDecoder(r.Body)
+  err := decoder.Decode(&payload)
+  defer r.Body.Close()
+  if err != nil {
+    http.Error(w, "Failed to parse request body", http.StatusBadRequest)
+    return
+  }
+
+  // Convert metadata to tags and populate the LogData struct
+  var data LogData
+  data.Tags = make(map[string]string)
+  for k, v := range payload {
+    if k != "message" {
+      data.Tags[k] = fmt.Sprintf("%v", v)
+    } else {
+      data.Message = fmt.Sprintf("%v", v)
+    }
+  }
+
+  // Check if the parser tag is available
+  parser, ok := data.Tags["parser"]
+  if ok {
+    values, err := f.Grok.Parse(parser, []byte(data.Message))
+    if err == nil {
+      for k, v := range values {
+        data.ParsedMessage[k] = v
+      }
+    }
   }
 
   // Convert the struct back to JSON for Kafka
@@ -94,6 +155,12 @@ func main() {
   }
   go startSyslogServer(g, &config)  // Start syslog server
 
+  // set up filebeatHandler and pass grok object
+  filebeatHandler := &FilebeatHandler{
+    Grok: g,
+  }
+
   http.HandleFunc("/ingest", ingestHandler)
+  http.HandleFunc("/filebeat", filebeatHandler.HandleRequest)
   log.Fatal(http.ListenAndServe(":8089", nil))
 }
